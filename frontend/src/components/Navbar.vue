951ab05ff7bcd46b26d3789773c982b97513bf32
juallybeans <jenis.onn.2023@scis.smu.edu.sg>
2025-03-21 16:15:03 +0800

    Added navbar changes

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -1,85 +1,77 @@
 <template>
-  <nav class="bg-white shadow-lg">
-    <div class="container mx-auto px-4">
-      <div class="flex justify-between items-center h-16">
-        <router-link 
-          :to="user ? '/restaurants' : '/'" 
-          class="text-2xl font-bold text-blue-600"
-        >
-          FoodDelivery
-        </router-link>
-        
-        <div class="flex items-center space-x-4">
-          <router-link to="/restaurants" class="hover:text-blue-600">
-            Restaurants
+  <div>
+    <nav class="bg-white shadow-lg">
+      <div class="container mx-auto px-4">
+        <div class="flex justify-between items-center h-16">
+          <router-link to="/" class="text-2xl font-bold text-blue-600">
+            FoodDelivery
           </router-link>
           
-          <div v-if="loading">Loading...</div>
-          <template v-else-if="user">
-            <router-link to="/cart" class="relative">
-              <vue-feather type="shopping-cart" size="24"></vue-feather>
-              <span 
-                v-if="items.length > 0" 
-                class="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs"
-              >
-                {{ items.length }}
-              </span>
+          <div v-if="user" class="flex items-center space-x-4">
+            <router-link to="/restaurants" class="hover:text-blue-600">
+              Restaurants
             </router-link>
             
-            <router-link to="/wallet">
-              <vue-feather type="credit-card" size="24"></vue-feather>
-            </router-link>
-            
-            <div class="relative">
-              <button 
-                ref="buttonRef"
-                @click="toggleMenu"
-                class="flex items-center focus:outline-none"
-                :aria-expanded="isMenuOpen"
-                aria-haspopup="true"
-              >
-                <vue-feather type="user" size="24" class="cursor-pointer"></vue-feather>
-              </button>
+            <div v-if="loading">Loading...</div>
+            <template v-else>
+              <router-link to="/cart" class="relative">
+                <vue-feather type="shopping-cart" size="24"></vue-feather>
+                <span 
+                  v-if="items.length > 0" 
+                  class="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs"
+                >
+                  {{ items.length }}
+                </span>
+              </router-link>
               
-              <div 
-                v-if="isMenuOpen" 
-                ref="menuRef"
-                class="absolute right-0 w-48 mt-2 bg-white rounded-md shadow-lg z-10"
-              >
-                <router-link 
-                  to="/profile" 
-                  class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-                  @click="handleMenuItemClick"
+              <router-link to="/wallet">
+                <vue-feather type="credit-card" size="24"></vue-feather>
+              </router-link>
+              
+              <div class="relative">
+                <button 
+                  ref="buttonRef"
+                  @click="toggleMenu"
+                  class="flex items-center focus:outline-none"
+                  :aria-expanded="isMenuOpen"
+                  aria-haspopup="true"
                 >
-                  Profile
-                </router-link>
-                <router-link 
-                  to="/orders" 
-                  class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-                  @click="handleMenuItemClick"
-                >
-                  My Orders
-                </router-link>
-                <button
-                  @click="handleLogout"
-                  class="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-100"
-                >
-                  Logout
+                  <vue-feather type="user" size="24" class="cursor-pointer"></vue-feather>
                 </button>
+                
+                <div 
+                  v-if="isMenuOpen" 
+                  ref="menuRef"
+                  class="absolute right-0 w-48 mt-2 bg-white rounded-md shadow-lg z-10"
+                >
+                  <router-link 
+                    to="/profile" 
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    @click="handleMenuItemClick"
+                  >
+                    Profile
+                  </router-link>
+                  <router-link 
+                    to="/orders" 
+                    class="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                    @click="handleMenuItemClick"
+                  >
+                    My Orders
+                  </router-link>
+                  <button
+                    @click="handleLogout"
+                    class="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-100"
+                  >
+                    Logout
+                  </button>
+                </div>
               </div>
-            </div>
-          </template>
-          <router-link
-            v-else
-            to="/login"
-            class="bg-blue-600 text-white px-4 py-2 rounded-md"
-          >
-            Login
-          </router-link>
+            </template>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </div>
 </template>
 
 <script setup>
